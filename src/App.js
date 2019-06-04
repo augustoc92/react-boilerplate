bac1fc3adb983ab8abccbc7a11551cb5f719e5ce
Augusto Camiletti <agust.92@gmail.com>
2019-06-04 17:22:59 +1000

    React Hot Loader

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React from 'react'
+import { hot } from 'react-hot-loader'
 
 class App extends React.Component {
   state = {
     count: 0
   }
 
+  increment = () => {
+    this.setState(state => ({count: this.state.count +1}))
+  }
+
+  decrement = () => {
+    this.setState(state => ({count: this.state.count +1}))
+  }
+
   render() {
-    debugger;
+    const { count } = this.state
+
     return (
       <div>
         <h1>
           Hello World!!
         </h1>
-        <h2>
-          Count: {this.state.count}
+        <h2 className={count > 10 ? 'warning' : null}>
+          Count: {count}
         </h2>
         <button
-          onClick={() => this.setState(state => ({count: this.state.count +1}))}
+          onClick={this.increment}
         >
           +
         </button>
         <button
-          onClick={() => this.setState(state => ({count: this.state.count -1}))}
+          onClick={this.decrement}
         >
           -
         </button>
@@ -29,5 +39,6 @@ class App extends React.Component {
     )
   }
 }
+// const hotFunction = hot(module)
 
-export default App
+export default hot(module)(App)
