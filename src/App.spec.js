18bd2a774c8e613526ef9f46f0c943de7489b6db
Augusto Camiletti <agust.92@gmail.com>
2019-06-05 17:00:49 +1000

    Jest global configuration file, updated deprecated

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import 'jest-dom/extend-expect'
-import '@testing-library/react/cleanup-after-each'
 import App from './App'
 
 describe('App', () => {
