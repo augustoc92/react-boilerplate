4a48ea176dd110bd98325e2c036675cf5ffd29b3
Augusto Camiletti <agust.92@gmail.com>
2019-06-11 08:57:49 +1000

    React strict mode, Default error boundary component.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,13 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import '@babel/polyfill'
 import './styles.css'
+import DefaultErrorBoundary from './DefaultErrorBoundary'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <React.StrictMode>
+    <DefaultErrorBoundary>
+      <App />
+    </DefaultErrorBoundary>
+  </React.StrictMode>,
+  document.getElementById('app')
+)
